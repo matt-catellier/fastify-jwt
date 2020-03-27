5f4493447dc3c0a59e6dc600fa5e4362ed24fd9b
Matt Catellier <matt@fournine.digital>
2020-03-27 11:26:43 -0700

    Added decorator to protect authenticated routes, added expiary date to token

--- index.js
+++ index.js
@@ -10,29 +10,77 @@ app.register(require('fastify-static'), {
   prefix: '/',
 })
 
-app.register(require('fastify-jwt'), {
-  secret: 'supersecret'
+app.register(require("fastify-jwt"), {
+  secret: "supersecret"
 })
 
+
+// app.register(require('./fastify-plugins/auth-strategy'))
+
+app.decorate("authenticate", async (request, reply)  => {
+  try {
+    await request.jwtVerify()
+  } catch (err) {
+    console.log(err)
+    reply.send(err)
+  }
+  return
+})
+
+const users = {
+  'admin': {
+    name: 'adam',
+    type: 'admin'
+  },
+  'manager': {
+    name: 'meaghan',
+    type: 'user'
+  },
+  'user': {
+    name: 'ulrich',
+    type: 'user'
+  },
+}
+
+app.get('/userInfo', async (req, reply) => {
+  const payload = req.user
+  console.log(payload)
+  console.log(users[payload.userId])
+  reply.send(users[payload.userId]) 
+})
 
 // ROUTES
 app.post('/signin', async (req, reply) => {
-  // some code
-  const payload = { name: 'matt' }
-  const token = app.jwt.sign({ payload })
+  console.log('sign in')
+  console.log(req.body)
+  const { id } = JSON.parse(req.body)
+  console.log(id)
+  const token = app.jwt.sign({ 
+    data: {id: req.body.id },
+    exp: Math.floor(Date.now() / 1000) + (10) // 10s
+  })
   reply.send({ token })
 })
 
+app.get('/protected',{ preValidation: [app.authenticate] },  async (req, reply) => {
+  reply.send('Only authorized can view')
+})
 
-app.get('/protected', async (req, reply) => {
-    await req.jwtVerify()
-    reply.send('Only authorized can view')
-  })
+app.get('/admin-only',{ preValidation: [app.authenticate] },  async (req, reply) => {
+  reply.send('Only authorized can view')
+})
 
-  app.get('/unprotected', (req, reply) => {
-    reply.send('Anyone can view')
-  })
-  
+app.get('/manager',{ preValidation: [app.authenticate] },  async (req, reply) => {
+  reply.send('Only manger & admin can view')
+})
+
+app.get('/user',{ preValidation: [app.authenticate] },  async (req, reply) => {
+  reply.send('Only user & admin can view')
+})
+
+app.get('/public', (req, reply) => {
+  reply.send('Anyone can view')
+})
 
 app.listen(3000, err => {
   console.log('app running on port 3000')
